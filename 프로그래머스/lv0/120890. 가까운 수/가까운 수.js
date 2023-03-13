a862face1b505c70720d7d3dd0b4c30507631a90
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-03-13 21:37:07 +0900

    [level 0] Title: 가까운 수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,11 +1,12 @@
 function solution(array, n) {
-    let min = 0;
-    let arr = array.sort((a, b) => a - b);
-    let answerArr = [];
+    let min = 0; // 최솟값 저장할 변수 선언
+    let arr = array.sort((a, b) => a - b); // input 배열을 오름차순으로 정렬
+    // why ? 만약 ([12, 9, 11], 10) 과 같이 들어올 경우, 더 작은 값을 출력하기 위함
+    let answerArr = []; // 절대값을 저장할 배열 변수 선언
     
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) { // 오름차순 정렬된 배열의 값을 for 루프 돌림
         answerArr.push(Math.abs(arr[i] - n));
-    }
-    min = Math.min(...answerArr);
-    return arr[answerArr.indexOf(min)];
+    } // 배열의 값 - n을 절대값으로 변환하여 answerArr 배열에 저장
+    min = Math.min(...answerArr); // answerArr 배열에 저장된 값 중 최솟값
+    return arr[answerArr.indexOf(min)]; // arr 배열에서 answerArr중 최솟값이 담긴 index의 값 출력
 }
