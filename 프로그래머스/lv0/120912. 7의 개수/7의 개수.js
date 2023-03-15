fd24566fc74807d196f702d3b4974b781319a678
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-03-15 19:51:58 +0900

    [level 0] Title: 7의 개수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,11 @@
+function solution(array) {
+    var answer = 0;
+    let str = array.join('');
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '7') {
+            answer += 1;
+        }
+    }
+    return answer;
+}
