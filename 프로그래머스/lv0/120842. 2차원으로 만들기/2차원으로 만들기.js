b7d91e5cf46274ef6f7c0ee591a269d290927a55
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-03-17 23:31:16 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,12 @@
+function solution(num_list, n) {
+    const answer = [];
+    for(let i = 0 ; i < num_list.length; ) {
+        const arr = [];
+        for(let j = 0 ; j < n ; j++) {
+            arr.push(num_list[i]);
+            i++;
+        }
+        answer.push(arr);
+    }
+    return answer;
+}
