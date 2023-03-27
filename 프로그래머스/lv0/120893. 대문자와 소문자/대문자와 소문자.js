28d9f5319a4095645a4fd78c4e683cbce8790e94
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-03-27 10:39:52 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,4 +1,10 @@
 function solution(my_string) {
     // 문자열 코드로 대문자는 65, 소문자는 97부터 시작
-    return [...my_string].map(v => v.charCodeAt() >= 65 && v.charCodeAt() < 97 ? v.toLowerCase() : v.toUpperCase()).join("")
+    return [...my_string] // 전개 연산자를 이용한 input 값 복사
+        .map(v => // map 메서드로 my_String의 값을 하나하나 받아, 새로운 배열로 만듬
+        v.charCodeAt() >= 65 && // charCodeAt을 이용하여 my_String 문자들의 문자열 코드                                    반환, 문자열 코드로 대문자는 65, 소문자는 97부터 시작함 
+        v.charCodeAt() < 97 ? 
+        v.toLowerCase() : // 삼항연산자를 이용하여 문자열 코드가 65이상, 97 미만인 문자는 Low                              erCase, 98 부터는 UpperCase 시켜줌
+        v.toUpperCase())
+        .join("") // map을 이용하여 배열로 반환된 값을 join을 통해 하나의 문자열로 변환
 }
