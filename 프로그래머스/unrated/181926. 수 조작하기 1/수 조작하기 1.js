b785b43358793e8c9c1c3560ce766c58ceb4b314
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-06-27 15:31:46 +0900

    [unrated] Title: 수 조작하기 1, Time: 4.39 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    const arr = control.split('')
+    
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] === 'w') {
+            n += 1;
+        } else if (arr[i] === 's') {
+            n -= 1;
+        } else if (arr[i] === 'd') {
+            n += 10;
+        } else
+            n -= 10;
+    }
+    return n;
+}
